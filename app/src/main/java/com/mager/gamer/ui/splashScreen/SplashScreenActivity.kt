e0c49ae86b08ba13cd6ca9b892415bf39fe5f7c8
Revangga Aji Pratama <revanggaajip2@gmail.com>
2022-03-31 21:12:07 +0700

    splashscreen

---
+++ app/src/main/java/com/mager/gamer/ui/splashScreen/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.mager.gamer.ui.splashScreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.mager.gamer.MainActivity
+import com.mager.gamer.databinding.ActivityMainBinding
+import com.mager.gamer.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(
+                this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+
+    }
+}
