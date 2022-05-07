f009cb6c43b16555a99d0966cfbd66ed6c4bbd3e
nasya zafira <nasya.zafira16@gmail.com>
2022-05-07 12:52:31 +0700

    fixing login ui

--- app/src/main/java/com/mager/gamer/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mager/gamer/ui/login/LoginActivity.kt
@@ -2,15 +2,13 @@ package com.mager.gamer.ui.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.lifecycleScope
 import com.mager.gamer.MainActivity
-import com.mager.gamer.R
 import com.mager.gamer.base.BaseActivity
 import com.mager.gamer.databinding.ActivityLoginBinding
 import com.mager.gamer.dialog.CustomLoadingDialog
-import com.mager.gamer.ui.home.HomeFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -26,17 +24,46 @@ class LoginActivity : BaseActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnMasuk.setOnClickListener {
-            if (binding.txtUsername.text != null && binding.txtPw.text != null) {
-                lifecycleScope.launch {
-                    viewModel.login(
-                        binding.txtUsername.text.toString(),
-                        binding.txtPw.text.toString()
-                    )
+        binding.textField.requestFocus()
+        binding.textField.error = "Email tidak boleh kosong"
+        binding.txtPassword.error = "Password tidak boleh kosong"
+
+        binding.txtUsername.doOnTextChanged { text, _, _, _ ->
+            when {
+                text.toString().isEmpty() ->
+                    binding.textField.error = "Email tidak boleh kosong"
+//                !Patterns.EMAIL_ADDRESS.matcher(text ?: "").matches() ->
+//                    binding.textField.error = "Format email tidak valid"
+                else -> binding.textField.error = null
+            }
+            validateButton()
+        }
+        binding.txtPw.doOnTextChanged { text, _, _, _ ->
+            when {
+                text.toString().isEmpty() -> {
+                    binding.txtPassword.error = "Password tidak boleh kosong"
                 }
+                else -> binding.txtPassword.error = null
+            }
+            validateButton()
+        }
+
+        binding.btnMasuk.setOnClickListener {
+            lifecycleScope.launch {
+                viewModel.login(
+                    binding.txtUsername.text.toString().trim(),
+                    binding.txtPw.text.toString().trim()
+                )
             }
         }
 
+        setupObserver()
+    }
+
+    private fun validateButton() {
+        val emailOK = binding.textField.error == null
+        val passOK = binding.txtPassword.error == null
+        binding.btnMasuk.isEnabled = emailOK && passOK
     }
 
     override fun setupObserver() {
@@ -45,7 +72,8 @@ class LoginActivity : BaseActivity() {
             if (it) loadingUi.show() else loadingUi.dismiss()
         }
         viewModel.errorLog.observe(this) {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            binding.textField.error = it
+//            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
         viewModel.succesData.observe(this) {
             val i = Intent(this, MainActivity::class.java)
