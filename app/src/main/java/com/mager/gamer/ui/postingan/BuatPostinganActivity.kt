aff1b07f8654c1d3313843bf82585a2ac63ca293
nasya zafira <nasya.zafira16@gmail.com>
2022-05-12 12:24:21 +0700

    add upload file

--- app/src/main/java/com/mager/gamer/ui/postingan/BuatPostinganActivity.kt
+++ app/src/main/java/com/mager/gamer/ui/postingan/BuatPostinganActivity.kt
@@ -1,18 +1,34 @@
 package com.mager.gamer.ui.postingan
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.anilokcun.uwmediapicker.UwMediaPicker
+import com.bumptech.glide.Glide
 import com.mager.gamer.databinding.ActivityBuatPostinganBinding
+import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
 
+@AndroidEntryPoint
 class BuatPostinganActivity : AppCompatActivity() {
     companion object {
         const val INTENT_IMAGE_MODE = "INTENT_IMAGE_MODE"
         const val INTENT_LIVE_MODE = "INTENT_LIVE_MODE"
     }
-    private lateinit var binding : ActivityBuatPostinganBinding
+
+    private lateinit var binding: ActivityBuatPostinganBinding
+    private val viewModel: DetailPostinganViewModel by viewModels()
     private var isImageMode = false
     private var isLiveMode = false
+    private var selectedFiles = mutableListOf<File>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBuatPostinganBinding.inflate(layoutInflater)
@@ -42,6 +58,10 @@ class BuatPostinganActivity : AppCompatActivity() {
             isLiveMode = false
             showHideMode()
         }
+
+        binding.layoutImage.setOnClickListener {
+            requestAccessForFile()
+        }
     }
 
     private fun showHideMode() {
@@ -65,5 +85,69 @@ class BuatPostinganActivity : AppCompatActivity() {
             binding.layoutLive.visibility = View.GONE
             binding.edtStatus.setLines(15)
         }
+    }
+
+    private fun requestAccessForFile() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                777
+            )
+        } else {
+            selectFileUpload()
+        }
+    }
+
+    private fun selectFileUpload() {
+        UwMediaPicker
+            .with(this)
+            .setGalleryMode(UwMediaPicker.GalleryMode.ImageGallery)
+            .setGridColumnCount(2)
+            .setMaxSelectableMediaCount(1)
+            .setLightStatusBar(true)
+            .setCompressFormat(Bitmap.CompressFormat.JPEG)
+            .setCompressionMaxWidth(709F)
+            .setCompressionMaxHeight(640F)
+            .enableImageCompression(true)
+            .setCompressionQuality(50)
+            .setCompressedFileDestinationPath(this@BuatPostinganActivity.filesDir.path)
+            .setCancelCallback {
+                selectedFiles.clear()
+            }
+            .launch { f ->
+                f?.let { files ->
+                    selectedFiles.clear()
+                    selectedFiles.addAll(files.map { File(it.mediaPath) })
+                    if (files.isNotEmpty())
+                        Glide.with(this).load(files[0].mediaPath).into(binding.imgPreview)
+                }
+            }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 777) {
+            if (
+                grantResults.isNotEmpty() && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                selectFileUpload()
+            } else {
+                Toast.makeText(
+                    this,
+                    "The app needs your permission",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
